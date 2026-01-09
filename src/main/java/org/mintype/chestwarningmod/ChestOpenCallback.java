ec159560f18fb9c966cac024fbe02f60748b211a
Shahrian Masud <mintype.code@gmail.com>
2026-01-08 23:55:30 -0500

    Add Warning

--- src/main/java/org/mintype/chestwarningmod/ChestOpenCallback.java
+++ src/main/java/org/mintype/chestwarningmod/ChestOpenCallback.java
@@ -4,6 +4,11 @@ import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.SubtitleS2CPacket;
+import net.minecraft.network.packet.s2c.play.TitleFadeS2CPacket;
+import net.minecraft.network.packet.s2c.play.TitleS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -20,9 +25,20 @@ public class ChestOpenCallback {
             Block block = world.getBlockState(hitResult.getBlockPos()).getBlock();
 
             if (block == Blocks.CHEST || block == Blocks.TRAPPED_CHEST) {
-                System.out.println("A player opened a chest!");
+                System.out.println("A player tried to open a chest!");
+
+                if (player instanceof ServerPlayerEntity serverPlayer) {
+                    serverPlayer.networkHandler.sendPacket(new TitleS2CPacket(Text.literal("Warning")));
+                    serverPlayer.networkHandler.sendPacket(new SubtitleS2CPacket(Text.literal("You opened a chest!")));
+                    serverPlayer.networkHandler.sendPacket(new TitleFadeS2CPacket(10, 60, 20));
+
+                    serverPlayer.sendMessage(Text.literal("Chest opened!"), true);
+                }
+
+                return ActionResult.FAIL;
             }
         }
         return ActionResult.PASS;
     }
+
 }
